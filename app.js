b601c42542fd612a8b32c42198f708ac17b1b568
Fabio Augusto <fasanrodri20@gmail.com>
2025-03-04 11:55:54 -0300

    adicionando rota de teste

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors'
-import 'dotenv/config'; 
+import 'dotenv/config';
 import { auth } from './routes/AuthRoutes.js';
 import { expenses } from './routes/ExpensesRoutes.js';
 
@@ -12,15 +12,18 @@ app.use(cors())
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Servidor Express com import está funcionando!');
+  res.send('Servidor Express com import está funcionando!');
 });
 app.use('/auth', auth)
 app.use('/despesas', expenses)
+app.use('/test', (req, res) => {
+  res.json({ message: "Login funcionando!" });
+})
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log('Conectado ao MongoDB'))
   .catch((err) => console.error('Erro ao conectar ao MongoDB:', err));
 
 app.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${port}`);
+  console.log(`Servidor rodando em http://localhost:${port}`);
 });
